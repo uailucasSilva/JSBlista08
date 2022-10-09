4df80efff5d4d86db3cefcd7b4552cfcf71c5923
uailucasSilva <107223850+uailucasSilva@users.noreply.github.com>
2022-10-09 15:54:23 -0300

    Delete Lista 08 directory

--- Lista 08/scripts/listarUsuario.js
+++
@@ -1,7 +0,0 @@
-function load() {
-    cadUsers = JSON.parse(localStorage.getItem("Usuário"));
-    if (cadUsers)
-        document.getElementById("mostraResult").innerHTML = `Usuário = ${cadUsers.nome} e Senha = ${cadUsers.senha}`;
-    else
-        document.getElementById("mostraResult").innerHTML = "Não há nenhum usuário cadastrado!";
-}
